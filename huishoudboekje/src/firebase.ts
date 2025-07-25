d94407cafb9fb1a2a9135c8eede066a73522ca27
JosVoogt2107 <163785523+JoV00gt@users.noreply.github.com>
2025-07-26 00:30:36 +0200

    Added firebase

---
+++ huishoudboekje/src/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZmSPFYBr5QYQVmtSyGTs0bQISrqdi_KU",
+  authDomain: "adweb-huishoudboekje-josvoogt.firebaseapp.com",
+  projectId: "adweb-huishoudboekje-josvoogt",
+  storageBucket: "adweb-huishoudboekje-josvoogt.firebasestorage.app",
+  messagingSenderId: "1002496861425",
+  appId: "1:1002496861425:web:c9a33b9426252237047550"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
