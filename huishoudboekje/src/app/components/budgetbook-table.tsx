5f9d5c2621718e36eaea88350649f025028c5815
JosVoogt2107 <163785523+JoV00gt@users.noreply.github.com>
2025-07-26 18:20:21 +0200

    aanpassingen voor tailwind

--- huishoudboekje/src/app/components/budgetbook-table.tsx
+++ huishoudboekje/src/app/components/budgetbook-table.tsx
@@ -1,7 +1,7 @@
 export function BudgetBookTable() {
     return (
     <div className="max-w-4xl mx-auto mt-10 p-4">
-        <h1 className="text-2xl font-bold mb-4">Mijn Huishoudboekjes</h1>
+        <h2 className="text-2xl font-bold mb-4">Mijn Huishoudboekjes</h2>
         <div className="overflow-x-auto bg-white shadow rounded-lg">
           <table className="min-w-full table-auto">
             <thead className="bg-gray-100 text-left">
--- huishoudboekje/src/app/dashboard/page.tsx
+++ huishoudboekje/src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import { BudgetBookTable } from '../components/budgetbook-table';
 export default function DashboardPage() {
   return (
     <div>
-      <h2 className="text-2xl font-semibold mb-4">Huishoudboekje</h2>
+      <h1 className="text-2xl font-semibold mb-4">Huishoudboekje</h1>
       <BudgetBookTable />
     </div>
   );
